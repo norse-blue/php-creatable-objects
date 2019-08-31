9b5ea8c89899cc71694930c3e72addb50f134446
Axel Pardemann <axel.pardemann@norse.blue>
2019-08-31 08:41:21 -0500

    :zap: Improve constructor resolver
    # Explain why this change is being made
    
    # Provide links to any relevant tickets, articles or other resources

--- src/Resolvers/ConstructorResolver.php
+++ src/Resolvers/ConstructorResolver.php
@@ -11,26 +11,31 @@ use ReflectionMethod;
 
 final class ConstructorResolver
 {
+    /** @var array<string, ReflectionMethod> */
+    private static $cache = [];
+
     /**
-     * Gets the class methods.
+     * Gets the class constructor reflection method.
      *
      * @param string $class
      *
-     * @return array<string, ReflectionMethod>
-     *
-     * @throws \ReflectionException
+     * @return \ReflectionMethod
      */
-    protected static function getClassMethods(string $class): array
+    private static function getConstructor(string $class): ReflectionMethod
     {
-        return array_reduce(
-            (new ReflectionClass($class))->getMethods(),
-            static function ($carry, $item) {
-                $carry[$item->name] = $item;
+        try {
+            $constructor = (new ReflectionClass($class))->getMethod('__construct');
+            // @codeCoverageIgnoreStart
+        } catch (ReflectionException $exception) {
+            throw new UnresolvableConstructorException(
+                'Cannot resolve constructor for class ' . $class . '.',
+                $exception->getCode(),
+                $exception,
+            );
+            // @codeCoverageIgnoreEnd
+        }
 
-                return $carry;
-            },
-            []
-        );
+        return $constructor;
     }
 
     /**
@@ -42,24 +47,10 @@ final class ConstructorResolver
      */
     public static function resolve(string $class): ReflectionMethod
     {
-        try {
-            $methods = self::getClassMethods($class);
-            // @codeCoverageIgnoreStart
-        } catch (ReflectionException $exception) {
-            throw new UnresolvableConstructorException(
-                'An error occurred while resolving constructor for class ' . $class . '.',
-                $exception->getCode(),
-                $exception,
-            );
-            // @codeCoverageIgnoreEnd
+        if (!array_key_exists($class, self::$cache)) {
+            self::$cache[$class] = self::getConstructor($class);
         }
 
-        if (!in_array('__construct', array_keys($methods))) {
-            throw new UnresolvableConstructorException(
-                'Cannot resolve constructor for class ' . $class . '.',
-            );
-        }
-
-        return $methods['__construct'];
+        return self::$cache[$class];
     }
 }
